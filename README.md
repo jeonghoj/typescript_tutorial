cc545825b3e565462123e086fb1d4e6ac0e09a3f
jeonghoj <jeonghoj94@gmail.com>
2019-12-29 13:50:10 +0900

    learn classes on typescript

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ JS는 엄격한 규칙이 없고 사용하기 쉽지만 큰 프로젝트나 팀�
 큰 프로젝트를 하거나 팀으로 일하거나 버그를 최소화 하기 위해 TypeScript 가 등장하였다.
 
 예측가능하고 읽기 쉬운 코드를 만드는데 도움이 된다.
+
+## interface vs class
+
+interface는 컴파일할때 나타나지 않지만,
+
+class는 컴파일하면 포함된다.
+
+interface는 typescript 측면에서는 안전하지만,
+
+react, express, node를 쓸때는 class를 사용해야한다.
+
+### classes on typescript
+
+타입스크립트를 이용한 class에서는 access modifier를 지원하여
+
+캡슐화를 할 수 있도록 해준다.
--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,21 +1,21 @@
-
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "jj",
-    age: 27,
-    gender: "male"
-}
+const jj = new Human("JJ", 27, "male")
 
 const sayHi = (person: Human): string => {
     return `hello ${person.name} ${person.age}, ${person.gender}`
 }
 
-console.log(sayHi(person))
+console.log(sayHi(jj))
 console.log("world")
 
 // gender is optional
